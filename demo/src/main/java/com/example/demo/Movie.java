7a84f4c08620598e1fa986f20903200d01a0b74c
p-cap <64616459+p-cap@users.noreply.github.com>
2020-07-22 11:46:38 -0700

    still need to work on adding the gross of each movie

---
+++ demo/src/main/java/com/example/demo/Movie.java
@@ -0,0 +1,99 @@
+package com.example.demo;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.context.annotation.Profile;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Movie {
+
+    private String title;
+    @JsonProperty("Title")
+    public String getTitle() { return title; }
+    @JsonProperty("Title")
+    public void setTitle(String title) { this.title = title; }
+
+    private int minutes;
+    @JsonProperty("Minutes")
+    public int getMinutes() { return minutes; }
+    @JsonProperty("Minutes")
+    public void setMinutes(int minutes) { this.minutes = minutes; }
+
+    private String genre;
+    @JsonProperty("Genre")
+    public String getGenre() { return genre; }
+    @JsonProperty("Menre")
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public double rating;
+    @JsonProperty("Rating")
+    public double getRating() { return rating; }
+    @JsonProperty("Rating")
+    public void setRating(double rating) { this.rating = rating; }
+
+    private int metascore;
+    @JsonProperty("Metascore")
+    public int getMetascore() { return metascore; }
+    @JsonProperty("Metascore")
+    public void setMetascore(int metascore) { this.metascore = metascore; }
+
+    private String description;
+    @JsonProperty("Description")
+    public String getDescription() { return description; }
+    @JsonProperty("Description")
+    public void setDescription(String description) { this.description = description; }
+
+    private int votes;
+    @JsonProperty("Votes")
+    public int getVotes() { return votes; }
+    @JsonProperty("Votes")
+    public void setVotes(int votes) { this.votes = votes; }
+
+    private double gross;
+    @JsonProperty("Gross")
+    public double getGross() { return gross; }
+    @JsonProperty("Gross")
+    public void setGross(double gross) { this.gross = gross; }
+
+    private int year;
+    @JsonProperty("Year")
+    public int getYear() { return year; }
+    @JsonProperty("Year")
+    public void setYear(int year) { this.year = year; }
+
+    private Credits[] credits;
+    @JsonProperty("Credits")
+    public Credits[] getCredits() { return credits; }
+    @JsonProperty("Credits")
+    public void setCredits(Credits[] credits) { this.credits = credits; }
+
+    static class Credits {
+
+        private Actors actor;
+
+        @JsonProperty("Person")
+        public Actors getActor() { return actor; }
+        @JsonProperty("Person")
+        public void setActor(Actors actor) { this.actor = actor; }
+
+        static class Actors {
+
+            private String role;
+            @JsonProperty("Role")
+            public String getRole() { return role; }
+            @JsonProperty("Role")
+            public void setRole(String role) { this.role = role; }
+
+            private String firstName;
+            @JsonProperty("FirstName")
+            public String getFirstName() { return firstName; }
+            @JsonProperty("FirstName")
+            public void setFirstName(String firstName) { this.firstName = firstName; }
+
+            private String lastName;
+            @JsonProperty("LastName")
+            public String getLastName() { return lastName; }
+            @JsonProperty("LastName")
+            public void setLastName(String lastName) { this.lastName = lastName; }
+        }
+    }
+}
---
+++ demo/src/main/java/com/example/demo/movieController.java
@@ -0,0 +1,83 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/movies")
+public class movieController {
+    @GetMapping("/movie")
+    //URL: localhost:8080/movies/movie
+    public Movie getMovies() {
+
+        Movie movie = new Movie();
+        movie.setTitle("The Godfather");
+        movie.setMinutes(175);
+        movie.setGenre("Crime, Drama");
+        movie.setRating(9.2);
+        movie.setMetascore(100);
+        movie.setDescription("The aging patriarch of an organized crime dynasty transfers control " +
+                "of his clandestine empire to his reluctant son.");
+        movie.setVotes(1561591);
+        movie.setGross(134.97);
+        movie.setYear(1972);
+
+        Movie.Credits[] credits = new Movie.Credits[5];
+
+        Movie.Credits.Actors actors = new Movie.Credits.Actors();
+        actors.setRole("Director");
+        actors.setFirstName("Francis Ford");
+        actors.setRole("Copolla");
+        credits[0] = new Movie.Credits();
+        credits[0].setActor(actors);
+        movie.setCredits(credits);
+
+        Movie.Credits.Actors actors1 = new Movie.Credits.Actors();
+        actors1.setRole("Star");
+        actors1.setFirstName("Marlon");
+        actors1.setLastName("Brnado");
+        credits[1] = new Movie.Credits();
+        credits[1].setActor(actors1);
+        movie.setCredits(credits);
+
+        Movie.Credits.Actors actors2 = new Movie.Credits.Actors();
+        actors2.setRole("Star");
+        actors2.setFirstName("Al");
+        actors2.setLastName("Pacino");
+        credits[2] = new Movie.Credits();
+        credits[2].setActor(actors2);
+        movie.setCredits(credits);
+
+        Movie.Credits.Actors actors3 = new Movie.Credits.Actors();
+        actors3.setRole("Star");
+        actors3.setFirstName("James");
+        actors3.setLastName("Caan");
+        credits[3] = new Movie.Credits();
+        credits[3].setActor(actors3);
+        movie.setCredits(credits);
+
+        Movie.Credits.Actors actors4 = new Movie.Credits.Actors();
+        actors4.setRole("Star");
+        actors4.setFirstName("Diane");
+        actors4.setLastName("Keaton");
+        credits[4] = new Movie.Credits();
+        credits[4].setActor(actors4);
+        movie.setCredits(credits);
+
+        return movie;
+    }
+
+    //still working this
+    @PostMapping("/gross/total")
+    public Map<String,Double> getTotalGross(@RequestBody() Array movies) {
+        double total = 0;
+
+        Map<String, Double> result = new HashMap<>();
+        result.put("result", total);
+        return result;
+
+    }
+} //end of movieController
